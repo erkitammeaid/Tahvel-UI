6c232f6f35bf35fa6b975d37455a09b4f3362c9a
Gen Vikat <47703789+vikatgen@users.noreply.github.com>
2020-10-02 11:45:56 +0300

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const profileButton = document.querySelector('.header-profile-wrapper');
-const profileModal = document.querySelector('.header-profile-modal');
-const dropdownArrow = document.querySelector('.dropdown-arrow');
-
-profileButton.addEventListener('click', () => {
-    profileModal.classList.toggle('open')
-    dropdownArrow.classList.toggle('rotate')
-})
